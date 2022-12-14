30f78a0f92ead16b39085f0ba2201b58f238f999
Edo <edocicak@gmail.com>
2022-12-14 09:55:00 +0100

    Added databse connection

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import { DUMMY, DUMMY_HEADER, categories } from "../const/const";
+import { DUMMY_HEADER, categories } from "../const/const";
 
 const express = require("express");
 const cors = require("cors");
@@ -34,32 +34,32 @@ app.use(express.json());
 
 // console.log(result.rows);
 
-// app.get("/articles", (req, res) => {
-//   pool.connect((err, client, done) => {
-//     if (err) throw err;
-//     client.query("SELECT * FROM articles", [], (err, result) => {
-//       done();
-//       if (err) throw err;
-//       let articles = result.rows;
-//       client.query(`SELECT * FROM comments`, [], (err2, result2) => {
-//         if (err2) throw err2;
-//         for (let article of articles) {
-//           let c = [];
-//           for (let comment of result2.rows) {
-//             if (comment.article_id == article.id) c.push(comment);
-//           }
-//           articles.find((x) => x.id == article.id)["comments"] = c;
-//         }
-//         res.send(articles);
-//       });
-//     });
-//   });
-// });
-
 app.get("/articles", (req, res) => {
-  res.status(200);
-  res.send(DUMMY);
+  pool.connect((err, client, done) => {
+    if (err) throw err;
+    client.query("SELECT * FROM articles", [], (err, result) => {
+      done();
+      if (err) throw err;
+      let articles = result.rows;
+      client.query(`SELECT * FROM comments`, [], (err2, result2) => {
+        if (err2) throw err2;
+        for (let article of articles) {
+          let c = [];
+          for (let comment of result2.rows) {
+            if (comment.article_id == article.id) c.push(comment);
+          }
+          articles.find((x) => x.id == article.id)["comments"] = c;
+        }
+        res.send(articles);
+      });
+    });
+  });
 });
+
+// app.get("/articles", (req, res) => {
+//   res.status(200);
+//   res.send(DUMMY);
+// });
 
 app.get("/categories", (req, res) => {
   res.status(200);
@@ -70,6 +70,8 @@ app.get("/head-article", (req, res) => {
   res.status(200);
   res.send(DUMMY_HEADER);
 });
+
+// app.post("/comment"), (req, res) => {};
 
 app.listen(8000, () => {
   console.log(`Server is running on port 8000.`);
