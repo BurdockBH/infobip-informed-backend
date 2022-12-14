679f222fb0bbb2f805ae2dbd1204c37d434a91be
BurdockBH <77117724+BurdockBH@users.noreply.github.com>
2022-12-14 13:41:22 +0100

    Added query for articles and comments (#1)
    * Added databse connection
    
    * Added query for articles
    
    * Added databse connection
    
    * Added databse connection
    
    * Deleted const, moves categories to be constant and in frontend. Head article is now id 0 in articles table.
    Todo: separate comments and articles for efficiency

--- server/server.ts
+++ server/server.ts
@@ -1,24 +1,59 @@
 const express = require("express");
 const cors = require("cors");
-import { DUMMY, DUMMY_HEADER, categories } from "../const/const";
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "cxksvmdq",
+  host: "mouse.db.elephantsql.com",
+  database: "cxksvmdq",
+  password: "L_iOJ28ZuXUxjow8-DSIQqroUb_nEE_H",
+  port: 5432,
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/articles", (req, res) => {
-  res.status(200);
-  res.send(DUMMY);
+  pool.connect((err, client, done) => {
+    if (err) throw err;
+
+    client.query("SELECT * FROM articles", [], (err, result) => {
+      if (err) throw err;
+      let articles = result.rows;
+      client.query(
+        `SELECT *
+           FROM comments`,
+        [],
+        (err2, result2) => {
+          if (err2) throw err2;
+          for (let article of articles) {
+            let c = [];
+            for (let comment of result2.rows) {
+              if (comment.article_id == article.id) c.push(comment);
+            }
+            articles.find((x) => x.id == article.id)["comments"] = c;
+          }
+          res.send(articles);
+          done();
+        }
+      );
+    });
+  });
 });
 
-app.get("/categories", (req, res) => {
-  res.status(200);
-  res.send(categories);
-});
-
-app.get("/head-article", (req, res) => {
-  res.status(200);
-  res.send(DUMMY_HEADER);
+app.post("/new-comment", (req, res) => {
+  pool.connect((err, client, done) => {
+    if (err) throw err;
+    const query = `INSERT INTO comments(content, time, article_id)
+                   VALUES ('${req.body.content}', '${req.body.time}', ${req.body.article_id})`;
+    if (err) throw err;
+    client.query(query, [], (err) => {
+      if (err) throw err;
+      res.send({ message: "Comment inserted" });
+      done();
+    });
+  });
 });
 
 app.listen(8000, () => {
